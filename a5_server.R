eecd3aff6a8ef51a2072b15d2a2c588758094e47
Roh Sarma <rohsarma@Rohs-MacBook-Air.local>
2022-12-11 23:43:51 -0800

    progress

---
+++ a5_server.R
@@ -0,0 +1,61 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  # Makes Dataframe of Cement CO2 Emissions since 1928  
+  agg_tbl1 <- a5_data %>%
+    group_by(year, country, cement_co2) %>%
+    drop_na(cement_co2) %>%
+    filter(cement_co2 > 0) %>%
+    filter(year > 1971) %>%
+    summarise(Occurrences = n())
+  
+  # Makes Dataframe of Cement C02 Emissions in Different Continents since 1928"
+  continents_cement_co2 <- agg_tbl1[agg_tbl1$country %in% c("Africa", "North America", "South America", "Europe", "Asia", "Oceania"),]
+  
+  #image
+  output$home_page <- renderImage({
+    list(src = "CO2.jpeg",
+         width = "100%",
+         height = 350)
+  },deleteFile = F)
+  
+  # Creates Interactive Bar Graph
+  output$selectContinent <- renderUI({
+    selectInput("Years", "Choose Year", choices = unique(continents_cement_co2$year))
+  })
+  barplot <- reactive({
+    chartdata <- continents_cement_co2 %>%
+      filter(year %in% input$Years)
+    chartdata2 <- chartdata %>%
+      arrange((cement_co2), by.group = TRUE)
+    ggplot(chartdata2, aes(x = reorder(country,-cement_co2), y = cement_co2, color = country, fill = country)) +
+      geom_col(width = 1) +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+      guides(fill=guide_legend(title="Continent")) +
+      labs(
+        x = "Continents",
+        y = "Cement CO2 Emissions in Million Tonnes",
+        title = "Cement CO2 Emissions in Million Tonnes By Continent Since 1972",
+        color = "")
+  })
+  output$barchart <- renderPlotly({
+    barplot()
+  }) 
+}
+  
+
+
+
+
---
+++ a5_ui.R
@@ -0,0 +1,64 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(dplyr)
+library(plotly)
+library(ggplot2)
+library(shinythemes)
+
+# Define UI for application that draws a histogram
+first_page <- tabPanel(
+  "Project Overview",
+  sidebarPanel(
+    h1(strong("About Me")),
+    hr(),
+    h4("Rohith Sarma"),
+    p("rsarma18@uw.edu"),
+    br(),
+  ),
+  mainPanel(
+    imageOutput("home_page"),
+    h2(strong("INTRODUCTION")),
+    br(),
+    p("In this interactive shiny web application, I wanted to focus on the carbon dioxide emissions that come from cement production. Instead of creating a barchart of emissions by country, I thought it would be interesting to create a barchart of emissions by continent. Since I think, that would give people a better idea of what parts of the world emit the most carbon dioxide through cement productions."),
+    br(),
+    h2(strong("VARIABLES")),
+    br(),
+    p("The variables that I used in this application are cement CO2 emissions, measured in million tonnes. The year range of 1971 through 2021. And the continents of Asia, Africa, Europe, North America, Oceania, and South Americ."),
+    br(),
+    h2(strong("VALUES")),
+    br(),
+    p("From this chart, I found several interesting things. The first is that in 1971, Europe had the highest cement CO2 emissions with around 161 million tonnes. The second is that in 2021, Asia had around 1359 million tonnes of CO2 emissions from cement production. Almost 8.5 times as much as Europe did in 1971. The third is that in 1971, Oceania only had around 2.9 million tonnes of CO2 emissions from cement production, and in 2021, it had increased to only 3.2 million tonnes.")
+  ))
+
+#2nd page, bar chart of disasters declared in different countries since 1970
+
+second_page <- tabPanel(
+  "Cement CO2 Emissions",
+  mainPanel(
+    titlePanel("Cement CO2 Emissions In Continents"),
+    sidebarPanel(
+      uiOutput("selectContinent")
+    ),
+    mainPanel(
+      plotlyOutput("barchart", width = 700, height = 500),
+      hr(),
+      h3(strong("About the Graph")),
+      p("This bargraph shows the carbon dioxide emissions for cement production in each continent, excluding Antarctica in the past 50 years. The graph is measured in million tonnes. This bar graph is an interactive graph where the user can pick any year since 1972 and see how things have changed. The bar graph is formatted in a way that for each year chosen, the graph will descend from left to right, in order of which continent produced the most CO2 emissions from cement production in a chosen year.")
+    )
+  )
+)
+
+ui <- navbarPage(
+  theme = shinytheme("flatly"),
+  "Carbon Dioxide Cement Emissions",
+  first_page,
+  second_page,
+)
